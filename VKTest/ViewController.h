ae6d9f44f10d2e75f40f66f7de77e447207bcd11
Александр <buga-91@inbox.ru>
2015-10-30 15:20:53 +0600

    initial commit

---
+++ VKTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKTest
+//
+//  Created by Александр on 30.10.15.
+//  Copyright © 2015 Alexander Bugaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VKTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VKTest
+//
+//  Created by Александр on 30.10.15.
+//  Copyright © 2015 Alexander Bugaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
